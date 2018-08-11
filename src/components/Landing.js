cdc5c6987d3bee3ef6ccf36afa358b55ba699919
Carlos Martinez <martinezcj2@gmail.com>
2018-08-11 03:14:44 -0400

    Temporarily remove components

---
+++ src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Fragment, Component } from 'react';
+import Header from './Header';
+import About from './About';
+import Examples from './Examples';
+import Travel from './Travel';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+class Landing extends Component {
+  render() {
+    return (
+        <div>this is the landing page</div>
+    );
+  }
+}
+
+export default Landing;
