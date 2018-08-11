66498c7f8b539b6658c8bbbc895d71b50b9f0288
Carlos Martinez <martinezcj2@gmail.com>
2018-08-11 03:14:13 -0400

    Change route to applied route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing';
 import NotFound from './components/NotFound';
+import AppliedRoute from './components/AppliedRoute';
 
-export default () => {
-  <Route component={NotFound} />
-}
+export default ({ childProps }) => 
+  <Switch>
+    <AppliedRoute exact path='/' component={Landing} props={childProps} />
+    <Route component={NotFound} />
+  </Switch>
