5876fba3a075aeb865030304b830f1a2b2d2a431
Carlos Martinez <martinezcj2@gmail.com>
2018-08-11 00:29:27 -0400

    Create route compnent

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from './components/NotFound';
+
+export default () => {
+  <Route component={NotFound} />
+}
